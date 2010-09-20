cakephp/cakephp

commit d5ffdc288d905d09ffc9ba1b900a9806a6ce6630

Including RequestHandler in controller now automatically sends proper content-type and encoding headers for html pages too like it does for other known file types. Closes #1099

cake/libs/controller/components/request_handler.php
@@ -230,13 +230,13 @@ class RequestHandlerComponent extends Object {
 
 		$this->__initializeTypes();
 		$controller->params['isAjax'] = $this->isAjax();
-		$isRecognized = (
-			!in_array($this->ext, array('html', 'htm')) &&
-			in_array($this->ext, array_keys($this->__requestContent))
-		);
 
-		if (!empty($this->ext) && $isRecognized) {
-			$this->renderAs($controller, $this->ext);
+		if (!empty($this->ext)) {
+			if (in_array($this->ext, array('html', 'htm'))) {
+				$this->respondAs('html', array('charset' => Configure::read('App.encoding')));
+			} elseif (in_array($this->ext, array_keys($this->__requestContent))) {
+				$this->renderAs($controller, $this->ext);
+			}
 		} elseif ($this->isAjax()) {
 			$this->renderAs($controller, 'ajax');
 		}

mtdmn/runking

commit 333d312901a93cde58eb10a8f8a765153926913c

Fixing regression cased by commit d5ffdc288d905d09ffc9ba1b900a9806a6ce6630 causing ajax layout to be not auto set for ajax request for html pages. Refs #1099

cake/libs/controller/components/request_handler.php
@@ -230,15 +230,17 @@ class RequestHandlerComponent extends Object {
 
 		$this->__initializeTypes();
 		$controller->params['isAjax'] = $this->isAjax();
+		$isRecognized = (
+			!in_array($this->ext, array('html', 'htm')) &&
+			in_array($this->ext, array_keys($this->__requestContent))
+		);
 
-		if (!empty($this->ext)) {
-			if (in_array($this->ext, array('html', 'htm'))) {
-				$this->respondAs('html', array('charset' => Configure::read('App.encoding')));
-			} elseif (in_array($this->ext, array_keys($this->__requestContent))) {
-				$this->renderAs($controller, $this->ext);
-			}
+		if (!empty($this->ext) && $isRecognized) {
+			$this->renderAs($controller, $this->ext);
 		} elseif ($this->isAjax()) {
 			$this->renderAs($controller, 'ajax');
+		} elseif (empty($this->ext) || in_array($this->ext, array('html', 'htm'))) {
+			$this->respondAs('html', array('charset' => Configure::read('App.encoding')));
 		}
 
 		if ($this->requestedWith('xml')) {

cake/tests/cases/libs/controller/components/request_handler.test.php
@@ -253,6 +253,27 @@ class RequestHandlerComponentTest extends CakeTestCase {
 		$this->assertEqual($this->Controller->ext, '.ctp');
 	}
 
+
+/**
+ * testAutoAjaxLayout method
+ *
+ * @access public
+ * @return void
+ */
+	function testAutoAjaxLayout() {
+		$_SERVER['HTTP_X_REQUESTED_WITH'] = 'XMLHttpRequest';
+		$this->RequestHandler->startup($this->Controller);
+		$this->assertTrue($this->Controller->layout, $this->RequestHandler->ajaxLayout);
+
+		$this->_init();
+		$this->Controller->params['url']['ext'] = 'js';
+		$this->RequestHandler->initialize($this->Controller);
+		$this->RequestHandler->startup($this->Controller);
+		$this->assertNotEqual($this->Controller->layout, 'ajax');
+
+		unset($_SERVER['HTTP_X_REQUESTED_WITH']);
+	}
+
 /**
  * testStartupCallback method
  *
@@ -692,7 +713,7 @@ class RequestHandlerComponentTest extends CakeTestCase {
 	}
 
 /**
- * assure that beforeRedirect with a status code will correctly set the status header 
+ * assure that beforeRedirect with a status code will correctly set the status header
  *
  * @return void
  */