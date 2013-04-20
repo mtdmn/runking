f83307f5ea28d5d5c9b947697388b968040213d0
mtdmn <mtdmn0@gmail.com>
2013-04-20 13:26:19 +0900

    file upload and DB insert completed.

--- app/Controller/RunpointsController.php
+++ app/Controller/RunpointsController.php
@@ -1,5 +1,5 @@
 <?php
-include_once('geoPHP/geoPHP.inc');
+include_once('GpxParser.php');
 
 class RunpointsController extends AppController {
     public $helpers = array('Html', 'Form', 'Session');
@@ -17,91 +17,38 @@ class RunpointsController extends AppController {
 
 	public function upload() {
         if ($this->request->is('post')) {
-			$tmp = $this->request->data['Upload']['file']['tmp_name'];
-			if(is_uploaded_file($tmp)) {
-				// check if the file is in GPX format. XXX
-				// retrieve runpoints extracted from the GPX file.
-				// save these runpoints to the DB.
-/*
-				$file_name = basename($this->request->data['Upload']['file']['name']);
-				$file = WWW_ROOT.'files'.DS.$file_name;
-				if (move_uploaded_file($tmp, $file)) {
-					$this->Upload->create();
-					$this->request->data['Upload']['file_name'] = $file_name;
-					if ($this->Upload->save($this->request->data)) {
-						$this->Session->setFlash(__('The upload has been saved'));
-						$this->redirect(array('action' => 'index'));
-					} else {
-						$this->Session->setFlash(__('The upload could not be saved. Please, try again.'));
-					}
-				}
-*/
+			$tmp = $this->request->data['Runpoint']['GPX']['tmp_name'];
+			// check file upload error.
+			if ($this->request->data['Runpoint']['GPX']['error']!=0) {
+				$this->Session->setFlash('File upload failed:'.
+					$this->request->data['Runpoint']['GPX']['error']);
+					break;
 			}
-			$this->Session->setFlash('Something is posted.');
-//                $this->redirect(array('action' => 'index'));
+
+			if(is_uploaded_file($tmp)) {
+				$value = file_get_contents($tmp);
+				// retrieve runpoints extracted from the GPX file.
+				$gpxp = new GpxParser($value);
+				$points = $gpxp->getRunpoints();
+				// save these runpoints to the DB.
+				foreach($points as $wkt) {
+					$this->Runpoint->create();
+					$this->Runpoint->set(
+						array(
+							'create_timestamp'=>DboSource::expression("NOW()"),
+//							'latlng'=>DboSource::expression('PointFromText("'.$wkt.'")')
+							'latlng'=>'POINT('.$wkt.')'
+							)
+						);
+					$this->Runpoint->save();
+				}
+			}
+			$this->Session->setFlash('GPX is uploaded.');
+			$this->redirect(array('action' => 'index'));
 		} else {
 			$this->Session->setFlash('Post your GPX file.');
 		}
 	}
 
-	public function upload_test() {
-		$wktarray = array(
-			'POINT(35.668 139.537)',
-			'POINT(35.669 139.537)',
-			'POINT(35.67 139.537)',
-			'POINT(35.667 139.538)',
-			'POINT(35.668 139.538)',
-			'POINT(35.669 139.538)'
-		);
-		foreach($wktarray as $wkt) {
-			$this->Runpoint->create();
-			$this->Runpoint->set(
-				array(
-					'create_timestamp'=>DboSource::expression("NOW()"),
-//					'latlng'=>DboSource::expression('PointFromText("'.$wkt.'")')
-					'latlng'=>$wkt
-					)
-				);
-			$this->Runpoint->save();
-		}
-
-		$this->Session->setFlash('upload completed.');
-		$this->redirect(array('action' => 'index'));
-	}
-
-    public function add() {
-        if ($this->request->is('post')) {
-            if ($this->Runpoint->save($this->request->data)) {
-                $this->Session->setFlash('Your Runpoint has been saved.');
-                $this->redirect(array('action' => 'index'));
-            } else {
-                $this->Session->setFlash('Unable to add your point.');
-            }
-        }
-    }
-
-	public function edit($id = null) {
-   	 $this->Runpoint->id = $id;
-   	 if ($this->request->is('get')) {
-   	     $this->request->data = $this->Runpoint->read();
-   	 } else {
-   	     if ($this->Runpoint->save($this->request->data)) {
-   	         $this->Session->setFlash('Your Runpoint has been updated.');
-   	         $this->redirect(array('action' => 'index'));
-   	     } else {
-   	         $this->Session->setFlash('Unable to update your Runpoint.');
-   	     }
-   	 }
-	}
-
-	public function delete($id) {
-   	 if ($this->request->is('get')) {
-    	    throw new MethodNotAllowedException();
-    	}
-    	if ($this->Runpoint->delete($id)) {
-        	$this->Session->setFlash('The Runpoint with id: ' . $id . ' has been deleted.');
-        	$this->redirect(array('action' => 'index'));
-    	}
-	}
 }
 ?>
