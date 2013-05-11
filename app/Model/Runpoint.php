917e1957a9ca246025e36aca7209538afb18728b
mtdmn <mtdmn0@gmail.com>
2013-05-11 17:19:35 +0900

    overwritten save and set is removed.

--- app/Model/Runpoint.php
+++ app/Model/Runpoint.php
@@ -2,26 +2,6 @@
 class Runpoint extends AppModel {
 	public $virtualFields = array('latlngtxt' => 'AsText(latlng)');
 
-/*
-	function set($params = array()) {
-		if (isset($params['latlng'])) {
-			$params['latlng'] = DboSource::expression('PointFromText("'. $params['latlng'].'")');
-		}
-
-		return parent::set($params);
-	}
-
-	// override
-	function save($data=null, $validate=true, $fieldList=array()) {
-
-		if (isset($data['Runpoint']['latlng'])) {
-			$data['Runpoint']['latlng'] = DboSource::expression('PointFromText("'. $data['Runpoint']['latlng'].'")');
-		}
-
-		return parent::save($data, $validate, $fieldList);
-	}
-	*/
-
 	function replaceinto($data=null, $validate=true, $fieldList=array()) {
 		$sql = "REPLACE INTO `runking`.`runpoints` (`create_timestamp`, `latlng`) VALUES (".$data['create_timestamp'].", PointFromText('".$data['latlng']."'))";
 
